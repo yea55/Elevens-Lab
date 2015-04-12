3275ef4ee67e7826bc46491d4fba7c358f156d0e
Cynthia He <cynthiacindycid@gmail.com>
2015-04-12 11:24:06 -0400

    Update Card.java

--- Card.java
+++ Card.java
@@ -87,6 +87,6 @@ public class Card {
   */
  @Override
  public String toString() {
-  return this.rank + "of" + this.suit + " (point value = " + this.pointValue + ")";
+  return this.rank + " of " + this.suit + " (point value = " + this.pointValue + ")";
  }
 }

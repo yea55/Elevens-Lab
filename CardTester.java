9d713b900fb92f86b0b7ecf213b65c12d216e36f
Cynthia He <cynthiacindycid@gmail.com>
2015-04-12 11:24:25 -0400

    Update CardTester.java

--- CardTester.java
+++ CardTester.java
@@ -8,22 +8,29 @@ public class CardTester {
   * @param args is not used.
   */
  public static void main(String[] args) {
-  Card card1 = new Card("Spade", "7", 7);
-  Card card2 = new Card("Spade", "7", 7);
-  Card card3 = new Card("Spade", "Queen", 11);
+  Card card1 = new Card("King", "Spades", 13);
+  Card card2 = new Card("King", "Spades", 13);
+  Card card3 = new Card("Queen", "Hearts", 12);
 
   System.out.println(card3.toString());
   
   System.out.println("Card 1 and Card 2 matches: " + card2.matches(card1));
   System.out.println("Card 2 and Card 3 matches: " + card2.matches(card3));
+  
   System.out.println("Card 1 Suit: " + card1.getSuit());
   System.out.println("Card 2 Suit: " + card2.getSuit());
   System.out.println("Card 3 Suit: " + card3.getSuit());
+  
   System.out.println("Card 1 Rank: " + card1.getRank());
   System.out.println("Card 2 Rank: " + card2.getRank());
   System.out.println("Card 3 Rank: " + card3.getRank());
+  
   System.out.println("Card 1 Point Value: " + card1.getPointValue());
   System.out.println("Card 2 Point Value: " + card2.getPointValue());
   System.out.println("Card 3 Point Value: " + card3.getPointValue());
+  
+  System.out.println("Card 1: " + card1.toString());
+  System.out.println("Card 2: " + card2.toString());
+  System.out.println("Card 3: " + card3.toString());
  }
 }
